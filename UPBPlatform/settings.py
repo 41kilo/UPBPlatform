fa197ef797c5f351b02c1eeb306a9b0c24aa2b33
windows <dve@dve78.ru>
2025-06-13 15:59:01 +0300

    1

--- UPBPlatform/settings.py
+++ UPBPlatform/settings.py
@@ -120,13 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ['http://62.183.54.224:8000']
