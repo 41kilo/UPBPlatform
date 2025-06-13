e640a0b9359539971cbcf769bc946be81d79fd7a
windows <dve@dve78.ru>
2025-06-13 14:18:16 +0300

    Добавлены настройки проекта, обновлены URL-адреса, изменены модели и админка, добавлены новые шаблоны и стили. Включены статические файлы и изображения для отзывов.

--- lk/admin.py
+++ lk/admin.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
-from .models import Tarif, Module, Lesson
-
-@admin.register(Tarif)
-class TarifAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
-    list_editable = ['price']
-    list_display_links = ['name']
+from .models import Module, Lesson
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ['name', 'tarif', 'open_date', 'close_date', 'slug']
+    list_display = ['name', 'open_date', 'close_date', 'slug']
     prepopulated_fields = {'slug': ('name',)}
-    list_editable = ['tarif', 'open_date', 'close_date']
+    list_editable = ['open_date', 'close_date']
     list_display_links = ['name']
 
 @admin.register(Lesson)
--- lk/models.py
+++ lk/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class Tarif(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'Тариф'
-        verbose_name_plural = 'Тарифы'
-    
-    def __str__(self):
-        return self.name
-
 class Module(models.Model):
-    tarif = models.ForeignKey(Tarif, related_name='modules', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -44,16 +29,3 @@ class Lesson(models.Model):
     
     def __str__(self):
         return self.name
-    
-    
-
-модуль
-название
-
-урок
-название
-модуль
-
-тариф
-название
-модуль 
