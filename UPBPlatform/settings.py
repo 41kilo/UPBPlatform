507276a59b84c3a2488667b821431453fe7406e2
windows <dve@dve78.ru>
2025-06-14 18:31:47 +0300

    123

--- UPBPlatform/settings.py
+++ UPBPlatform/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'UPBPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'u3163482_UPBPlatform',  # Имя базы данных из панели управления Reg.ru
+        'USER': 'u3163482_default',  # Пользователь базы данных из панели управления
+        'PASSWORD': 'bB5458xksMZR5Wzo',  # Пароль из панели управления
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
