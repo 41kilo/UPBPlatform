0594a1a4af5de848eaea503cf924631ce112ffae
windows <dve@dve78.ru>
2025-06-17 04:30:57 +0300

    Изменен путь для STATIC_ROOT в настройках проекта на '../static' для корректного размещения статических файлов.

--- UPB/settings.py
+++ UPB/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Создайте новую папку staticfiles
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')  # Создайте новую папку staticfiles
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
